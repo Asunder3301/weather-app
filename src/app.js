cf41de2923ca42e3a88bc0184a678037eb488edf
Asunder <trashemail3301@gmail.com>
2025-03-17 12:03:07 -0800

    build: display info
    When button is clicked the temperature, day, and the conditions will be displayed on the webpage and logged into the console.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 async function fetchWeatherData(location) {
     const apiKey = 'JA4BM99882BW8TJF7AL5YERHB';
     const baseUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
+    const info = document.getElementById('weather-info');
     
     try {
         const response = await fetch(`${baseUrl}${location}?key=${apiKey}`);
         const data = await response.json();
-        const weatherData = data.currentConditions
-        console.log(weatherData);
+        const temperature = data.currentConditions.temp;
+        const conditions = data.currentConditions.conditions;
+        const icon = data.currentConditions.icon
+        console.log(`Temperature: ${temperature}
+                    Conditions: ${conditions}
+                    Day: ${icon}`);
+        info.innerHTML = `
+                    <div>Temperature: ${temperature}</div>
+                    <div>Conditions: ${conditions}</div>
+                    <div>Day: ${icon}</div>    
+                    `;
     } catch (error) {
         console.log('Error: fetching weather data:', error);
     }
